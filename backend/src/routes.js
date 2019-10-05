1a1f8beed348e24066daff0a15c6946a47b54a8e
MatheusAraujo42 <matheus.araujo@corebiz.com.br>
2019-10-05 00:25:03 -0300

    Dashboard show

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -15,5 +16,7 @@ const upload = multer(uploadConfig);
 routes.post('/store', SessionController.store);
 routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+routes.get('/dashboard', DashboardController.show);
 
 module.exports = routes;
